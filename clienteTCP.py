f744a459c45750332fe4894fa59335afa5360af5
Daniel Aragão Abreu Filho <danielfilhoce@outlook.com>
2017-10-02 21:34:18 -0300

    Servidor tcp e cliente tcp se comunicam até digitar .

---
+++ clienteTCP.py
@@ -0,0 +1,20 @@
+import socket
+
+
+ip = '192.168.0.4'
+porta = 12001
+mensagem = ''
+
+
+while mensagem.find('.') < 0:
+    socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketCliente.connect((ip,porta))
+    mensagem = raw_input("Digite uma mensagem: ")
+
+    socketCliente.send(mensagem)
+    resposta = socketCliente.recv(2048)
+    print resposta
+
+    socketCliente.close()
+
+
